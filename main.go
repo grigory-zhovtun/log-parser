af1099d366a3b924af57a8676d5daf704d9eb8d7
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-06 10:46:15 +0500

    feat(SENT-003): create a generator log files

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	g "log_parser/generator"
+	gen "log_parser/generator"
 )
 
 func main() {
 	fmt.Println("Log Parser v.0.1")
 
-	g.Generator()
+	gen.Generator()
 	// fmt.Println(g.Get_randome_IP())
 }
