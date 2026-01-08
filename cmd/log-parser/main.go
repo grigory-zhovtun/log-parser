7f5797d808dea631557a41521459d5a9952a6fa3
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-08 23:24:32 +0500

    feat(SENT-006): add scanner for access.log

--- cmd/log-parser/main.go
+++ cmd/log-parser/main.go
@@ -1,7 +1,34 @@
-package main
+package logparser
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Log Parser v.0.1")
+
+	file, err := os.Open("access.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	count := 0
+	for scanner.Scan() {
+		count++
+		if count%10000 == 0 {
+			fmt.Println(count)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("общее количество строк: %d\n", count)
 }
