290e43d938b33325b568e97102af89704a1f8708
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-09 16:32:19 +0500

    feat(SENT-007): create func ParseLogLine

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,10 +1,39 @@
 package parser
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 // LogEntry представляет одну запись лога Nginx
 type LogEntry struct {
-    IP         string
-    Timestamp  string
-    Method     string
-    Path       string
-    StatusCode int
+	IP         string
+	Timestamp  string
+	Method     string
+	Path       string
+	StatusCode int
+}
+
+func ParseLogLine(line string) (LogEntry, error) {
+	re := regexp.MustCompile(`^(\S+) - \[(.+?)\] "(\S+) (\S+) .*?" (\d+)`)
+
+	matches := re.FindStringSubmatch(line)
+
+	if len(matches) < 6 {
+		fmt.Println("Ошибка: формат строки не совпадает")
+		return
+	}
+
+	statusCode, _ := strconv.Atoi(matches[5])
+
+	entry := LogEntry{
+		IP:         matches[1],
+		Timestamp:  matches[2],
+		Method:     matches[3],
+		Path:       matches[4],
+		StatusCode: statusCode,
+	}
+
+	return entry, nil
 }
