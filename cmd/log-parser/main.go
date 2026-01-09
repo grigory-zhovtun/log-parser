db455bd63590a9814da470e23e4b3b4ba71bdb94
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-09 21:56:38 +0500

    feat(SENT-008): use ParseLogLine in main func

--- cmd/log-parser/main.go
+++ cmd/log-parser/main.go
@@ -3,22 +3,43 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"log-parser/pkg/parser"
 )
 
 func main() {
-	file, _ := os.Open("access.log")
+	fmt.Println("Log Parser v.0.1")
+
+	file, err := os.Open("access.log")
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
+
+	errorCount := 0
 	successCount := 0
+
 	for scanner.Scan() {
-		_, err := parser.ParseLogLine(scanner.Text())
+		line := scanner.Text()
+		_, err := parser.ParseLogLine(line)
 		if err != nil {
+			errorCount++
 			continue
 		}
+
 		successCount++
+		if successCount%10000 == 0 {
+			fmt.Println(successCount)
+		}
 	}
-	fmt.Printf("Parsed: %d\n", successCount)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Success: %d, Errors: %d\n", successCount, errorCount)
 }
